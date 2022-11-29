c274a3de685c878931a007c505b984bd6c8e2d16
ksm <ksm@cudo.co.kr>
2022-11-29 11:14:55 +0900

    init

---
+++ src/main/java/com/nepnep/NepnepWikiApplication.java
@@ -0,0 +1,13 @@
+package com.nepnep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepnepWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepnepWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nepnep/NepnepWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepnep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepnepWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
