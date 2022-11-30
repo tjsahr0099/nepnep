da37db2037366706b06d4dd42f750083200bc296
ksm <ksm@cudo.co.kr>
2022-11-30 14:45:57 +0900

    URL 사용기준 생성

---
+++ src/main/java/com/nepnep/wiki/common/URLs.java
@@ -0,0 +1,12 @@
+package com.nepnep.wiki.common;
+
+public class URLs {
+
+	private final static String prefix = "/api";
+	
+	public final static String USER = prefix + "/user";
+	public final static String CATEGORY = prefix + "/category";
+	public final static String WIKI_CONTENTS = prefix + "/wikicont";	
+	
+	
+}
