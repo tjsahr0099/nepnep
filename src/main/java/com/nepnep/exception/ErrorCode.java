cbe2c1b228ee867fefbfca9614354f4947c24db7
ksm <ksm@cudo.co.kr>
2022-12-02 15:39:06 +0900

    spring security, jwt 를 이용한 인증 개발

--- src/main/java/com/nepnep/exception/ErrorCode.java
+++ src/main/java/com/nepnep/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
 
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND,"유저 정보를 찾을 수 없습니다."),
 	
-	TOKEN_EXPIRED(HttpStatus.FORBIDDEN,"만료된 토큰입니다.")
+	TOKEN_EXPIRED(HttpStatus.FORBIDDEN,"만료된 토큰입니다."),
+	TOKEN_INVALID(HttpStatus.FORBIDDEN,"잘못된 토큰입니다."),
+	TOKEN_EMPTY(HttpStatus.FORBIDDEN,"토큰이 없습니다.")
 	
 	;
 	
--- src/main/java/com/nepnep/security/SecurityConfig.java
+++ src/main/java/com/nepnep/security/SecurityConfig.java
@@ -4,29 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	/**
+	 * https://gksdudrb922.tistory.com/217#MemberRepository
+	 */
+
+	private final JwtTokenProvider jwtTokenProvider;
 
 	@SuppressWarnings("deprecation")
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
-		 http
-         .csrf().disable()
-         .headers().frameOptions().disable()
-         .and().authorizeRequests() 
-         .requestMatchers("/api/**").permitAll()  
-         .anyRequest().authenticated()
-         .and().addFilter(null)
-//         .logout()
-//         .logoutSuccessUrl("/")
-         ;
 
-         return http.build();
+		http.httpBasic().disable().csrf().disable()
+//		.headers().frameOptions().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+				.requestMatchers("/api/**").permitAll().anyRequest().authenticated().and().addFilterBefore(
+						new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+//				.and().logout().disable();
+
+		return http.build();
 	}
-	
-	
+
 }
