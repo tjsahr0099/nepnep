ca7e630d845446a93ef5528bc8c2339440800807
tjsahr0099 <dearelfoov@gmail.com>
2022-12-17 22:33:51 +0900

    쿼리

--- src/main/java/com/nepnep/algorithm/ksm/신고_결과_받기.java
+++ src/main/java/com/nepnep/algorithm/ksm/신고_결과_받기.java
@@ -1,6 +1,7 @@
 package com.nepnep.algorithm.ksm;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -16,13 +17,25 @@ public class 신고_결과_받기 {
 
 	public static void main(String[] args) {
 		
-		String[] id_list = null;
-		String[] report = null;
-		int k = 0;
+		String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+		int k = 2;
+		
+//		String[] id_list = {"ryan", "con"};
+//		String[] report = {"ryan con","ryan con","ryan con","ryan con"};
+//		int k = 3;
+//		
+//		String[] id_list = {"a", "b", "c", "d"};
+//		String[] report = {"a b","c b"};
+//		int k = 2;
 		
 		Solution2 s = new Solution2();
-		s.solution(id_list, report, k);
-
+		int[] answer = s.solution2(id_list, report, k);
+		
+		for(int i=0;i<answer.length;i++) {
+			System.out.println(answer[i]);
+		}
+		
 	}	
 
 }
@@ -31,12 +44,15 @@ public class 신고_결과_받기 {
 class Solution2 {
 
 	public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
         
-        Map<String,Integer> result = new LinkedHashMap<String,Integer>();
+        Map<String,Integer> reportCnt = new LinkedHashMap<String,Integer>();
+        Map<String,Integer> mailCnt = new LinkedHashMap<String,Integer>();
         
+        // 0개로 초기화
         for (String id : id_list) {
-        	result.put(id, 0);
+        	reportCnt.put(id, 0);
+        	mailCnt.put(id, 0);
         }
         
         // 중복 제거
@@ -45,23 +61,87 @@ class Solution2 {
                 .distinct()
                 .collect(Collectors.toList());
         
-        for (String r : reportList) {
+        List<String> reportList2 = reportList;
+        
+        // 신고당한 사람의 카운트 구하기
+        for (String r : reportList) {        	
         	
-        	//신고한 사람
-			String from = r.split(" ")[0];
-			
 			//신고당한 사람
 			String to = r.split(" ")[1];
 			
-			Integer toVal = result.get(to);
-			result.replace(to, toVal, toVal+1);		
+			Integer toVal = reportCnt.get(to)==null?0:reportCnt.get(to);
+			Integer nextVal = ++toVal;
+			reportCnt.replace(to, nextVal);	
+			
+			// 신고 횟수가 정지 횟수에 도달하면 메일 카운트 증가
+			if(nextVal==k) {
+				for (String r2 : reportList2) {
+					
+					//신고한 사람
+					String from = r2.split(" ")[0];	
+					
+					//신고당한 사람
+					String to2 = r2.split(" ")[1];
+					
+					// 신고한 사람을 모두 찾아 카운트 1씩 증가
+					if(to2.equals(to)) {
+						mailCnt.put(from, mailCnt.get(from)+1);
+					}
+					
+				}
+			}
 			
 		}
-        
-        for (String id : id_list) {
-        	result.put(id, 0);
+
+       
+        int i=0;
+        for (String key : mailCnt.keySet()) {
+
+        	answer[i]=mailCnt.get(key);
+        	i++;
         }
         
+        return answer;
+    }
+	
+	int matchCnt = 0;
+	
+	public int[] solution2(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        Map<String,String> mailing = new LinkedHashMap<String,String>();
+        
+        
+        for(String reportOne : report) {
+        	
+        	String f = reportOne.split(" ")[0];
+        	String t = reportOne.split(" ")[1];
+        	
+        	String _f = mailing.get(t)==null ? "" : mailing.get(t);
+        	if(!_f.matches("(.*)"+f+"(.*)")) {
+        		mailing.put(t,(_f+" "+f).trim());	
+        	}        	
+        	
+        }
+        
+        int i=0;
+        for(String id : id_list) {
+        	String str = mailing.get(id) == null ? "" : mailing.get(id);
+        	
+        	// 신고 정지 기준 체크
+        	matchCnt=0;
+        	mailing.forEach((key,value)->{
+        		if(value.split(" ").length >= k) {
+            	
+        			if(value.matches("(.*)"+id+"(.*)")) {
+        				matchCnt++;        				
+        			}
+            	}
+        	});
+        	answer[i]=matchCnt;
+        	i++;
+        	
+        }        
         
         return answer;
     }
--- src/main/java/com/nepnep/algorithm/ksm/햄버거_만들기.java
+++ src/main/java/com/nepnep/algorithm/ksm/햄버거_만들기.java
@@ -1,10 +1,83 @@
 package com.nepnep.algorithm.ksm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=java
- * @author ì‚¬ìš©ì
+ * @author »ç¿ëÀÚ
  *
  */
-public class í–„ë²„ê±°_ë§Œë“¤ê¸° {
+public class ÇÜ¹ö°Å_¸¸µé±â {
+	
+	public static void main(String[] args) {
+		
+		int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+		
+		Solution3 s = new Solution3();
+		int result = s.solution(ingredient);
+		
+		System.out.println(result);
+	}
 
+}
+
+class Solution3 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        
+        String str = "";
+        
+        for(int i=0;i<ingredient.length;i++) {        	
+        	str+=ingredient[i];  	
+        } 
+        
+        Map<String,Object> answerMap = new HashMap<String,Object>();
+        answerMap.put("ingredient_str", "@"+str+"@");
+        answerMap.put("answer", 0);
+        
+        while(true) {
+        	answerMap = roof(answerMap);
+//        	System.out.println(answerMap.toString());
+        	if(answerMap.get("exit")!=null && (boolean) answerMap.get("exit")) {
+        		break;
+        	}
+        }
+        
+        
+        
+        return (int) answerMap.get("answer");
+    }
+    
+    public Map<String,Object> roof( Map<String,Object> input) {
+    	
+//    	System.out.println(input.toString());
+    	
+    	String[] strArray = input.get("ingredient_str").toString().split("1231");
+    	int length = strArray.length;
+    	
+    	String str = "";
+    	
+    	Map<String,Object> res = new HashMap<String,Object>();
+        
+//    	System.out.println("length : " + length);
+    	
+        if(length<=1) {
+        	res.put( "answer", (int)input.get("answer"));
+        	res.put( "exit", true );
+        }else {
+        	
+        	for(int i=0;i<length;i++) {        	
+            	str+=strArray[i];  	
+            }
+        	
+        	
+        	res.put( "answer", (int)input.get("answer")+(length-1) );
+        	res.put( "ingredient_str", str );
+        
+        	
+        }
+        
+        return res; 
+    }
 }
