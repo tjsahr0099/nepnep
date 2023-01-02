cfb3d1ca26d245e3ec99358cc0fcfd80878a53a1
ksm <ksm@cudo.co.kr>
2023-01-03 08:45:49 +0900

    숫자짝꿍 init

---
+++ src/main/java/com/nepnep/algorithm/숫자짝꿍.java
@@ -0,0 +1,20 @@
+package com.nepnep.algorithm;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/131128
+ * @author ksm
+ * 2023. 1. 3.
+ */
+public class 숫자짝꿍 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String solution(String X, String Y) {
+        String answer = "";
+        return answer;
+	}
+	 
+}
