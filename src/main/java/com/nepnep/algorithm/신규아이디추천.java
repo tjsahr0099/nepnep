a5b3bc421d4c73e525b53a7b00e7aaeafd79dc45
ksm <ksm@cudo.co.kr>
2023-01-03 08:44:28 +0900

    기사단원 제거 신규아이디추천 init

--- src/main/java/com/nepnep/algorithm/신규아이디추천.java
+++ src/main/java/com/nepnep/algorithm/신규아이디추천.java
@@ -1,20 +1,20 @@
 package com.nepnep.algorithm;
 
 /**
- * https://school.programmers.co.kr/learn/courses/30/lessons/136798
+ * https://school.programmers.co.kr/learn/courses/30/lessons/72410
  * @author ksm
  * 2023. 1. 3.
  */
-public class 기사단원의무기 {
+public class 신규아이디추천 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
-	public int solution(int number, int limit, int power) {
-        int answer = 0;
+	
+	public String solution(String new_id) {
+        String answer = "";
         return answer;
     }
-	
+
 }
