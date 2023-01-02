df9d4f28ef4520a05a16f809af98dc6275f1d363
ksm <ksm@cudo.co.kr>
2023-01-03 08:42:45 +0900

    기사단원의무기 init

---
+++ src/main/java/com/nepnep/algorithm/기사단원의무기.java
@@ -0,0 +1,20 @@
+package com.nepnep.algorithm;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/136798
+ * @author ksm
+ * 2023. 1. 3.
+ */
+public class 기사단원의무기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+	
+}
