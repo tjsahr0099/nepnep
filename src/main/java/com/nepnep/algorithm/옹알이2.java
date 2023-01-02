6a0f1fd05580ee2ba8f02cc2f7b379bf282610ba
ksm <ksm@cudo.co.kr>
2023-01-03 08:41:04 +0900

    옹알이2 init

---
+++ src/main/java/com/nepnep/algorithm/옹알이2.java
@@ -0,0 +1,24 @@
+package com.nepnep.algorithm;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/133499
+ * @author ksm
+ * 2023. 1. 3.
+ */
+public class 옹알이2 {
+
+	public static void main(String[] args) {
+		
+		String[] babbling = {};
+		
+		옹알이2 s = new 옹알이2();
+        s.solution(babbling);
+
+	}
+	
+	public int solution(String[] babbling) {
+		int answer = 0;
+        return answer;
+    }
+
+}
