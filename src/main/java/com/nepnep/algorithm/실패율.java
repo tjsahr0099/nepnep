72db482485604e64f7133d1c8d2f2cbe6ccfa1a5
tjsahr0099 <dearelfoov@gmail.com>
2023-02-02 22:07:23 +0900

    모르겟다

--- src/main/java/com/nepnep/algorithm/실패율.java
+++ src/main/java/com/nepnep/algorithm/실패율.java
@@ -1,32 +1,32 @@
 package com.nepnep.algorithm;
 
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class 실패율 {
 
 	public static void main(String[] args) {
-		
-		
-		
+						
 		int N = 5;
 		int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
 		
-		new 실패율().solution(N, stages);
+		int[] answer = new 실패율().solution(N, stages);
+		
+		for(int x : answer) {
+			System.out.println(x);
+		}
 	}
 	
 	public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
         
-//        int base = 0;
-//        for(int i=1 ; i<=N ; i++) {
-//        	
-//        }
-        
-        List<String> list = new ArrayList<String>();
-        
         int sum = 0;
+        HashMap<Integer,Float> map = new HashMap<Integer,Float>();
+        
         for(int i=1 ; i<=N+1 ; i++) {
         	int cnt = 0;
         	for(int s : stages) {
@@ -35,28 +35,43 @@ public class 실패율 {
         		}
         	}
         	if(i!=1) {
-        		String a = String.format("%.6f", (float)cnt/(stages.length-sum)) + ( (N+1) - i );
-        		list.add(a);
-//        		int a = Math.round(cnt/(stages.length-sum)*100000000);
-//        		System.out.println("cnt:" + cnt + " , " + "sum:" + (stages.length-sum) + " cal:" + a);
-//        		System.out.println("cnt:" + cnt + " , " + "sum:" + (stages.length-sum));
+        		
+        		float f = (float)cnt/(stages.length-sum);
+        		System.out.println(f);
+        		map.put(i-1, f);        		
+
         	}
         	sum = sum + cnt;        	
-//        	System.out.println("cnt:" + cnt + " , " + "sum:" + (stages.length-sum));
+
         }
         
-        list.sort(Comparator.reverseOrder());
-        
-        int i = 0;
-        for(String s : list) {
-        	String s_last = s.substring(s.length()-1);
-        	answer[i] = N- Integer.parseInt(s_last);
-        	i++;
+       
+        HashMap<Integer,Float> hashMap = sortByValue(map);
+        int i=0;
+        for(Integer key : hashMap.keySet()){        	 
+ 
+            System.out.println(key);
+            
+            answer[i++] = key;
+ 
         }
-        
-//        System.out.println(list);
-        
+
         return answer;
     }
+	
+	public HashMap<Integer,Float> sortByValue(HashMap<Integer,Float> hm){
+	    // Create a list from elements of HashMap
+	    List<Map.Entry<Integer,Float> > list = new LinkedList<Map.Entry<Integer,Float> >(hm.entrySet());
+
+	    // Sort the list using lambda expression
+	    Collections.sort(list, (i2, i1) -> i1.getValue().compareTo(i2.getValue()));
+
+	    // put data from sorted list to hashmap
+	    HashMap<Integer,Float> temp = new LinkedHashMap<Integer,Float>();
+	    for (Map.Entry<Integer,Float> aa : list) {
+	        temp.put(aa.getKey(), aa.getValue());
+	    }
+	    return temp;
+	}
 
 }
